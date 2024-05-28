3baedae5c6cdb3b6fd98caebf0c76268bf5d34c8
AkeemKaleeb <kaileb.j.cole@gmail.com>
2024-05-28 00:39:59 -0400

    Hello Cargo Commit
    Upload of learning to use Cargo

---
+++ projects/variables/target/debug/deps/variables-87cb5ec98b5133fc.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/kaile/Desktop/Programming/Programs/Rust/Rust Book/LearnRust/projects/variables/target/debug/deps/variables-87cb5ec98b5133fc: src/main.rs
+
+/mnt/c/Users/kaile/Desktop/Programming/Programs/Rust/Rust Book/LearnRust/projects/variables/target/debug/deps/variables-87cb5ec98b5133fc.d: src/main.rs
+
+src/main.rs:
